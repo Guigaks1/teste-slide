f4622c195e24e5fe93f23aa31f5fa5469b44601d
Guigaks1 <73757598+Guigaks1@users.noreply.github.com>
2020-11-16 19:52:56 -0300

    Add files via upload

---
+++ curso-react-redux-master/script/java.js
@@ -0,0 +1,9 @@
+/*var nome = window.prompt('Qual é o seu nome?') //vai perguntar o nome
+window.alert('È um gante prazer em te conhecer,' + nome + 'Tudo bem ? ')
+var n1 = Number.parseInt(window.prompt('digite un numero'))
+var n2 = Number.parseInt(window.prompt('digite outro numero'))
+var s = n1 + n2
+window.alert('A soma dos valores é ' + s)*/
+
+const test = document.querySelector('#teste')
+console.log(test)
